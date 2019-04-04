b2206b4812d0d7b4526f94473246d2f08b8f07b9
esafif <esafif637@gmail.com>
2019-04-04 17:14:23 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#tableData').DataTable( {
+    ajax: {
+        url: 'https://swapi.co/api/planets/2',
+        dataSrc: 'results'
+    },
+        columns: [
+            { data: 'name' },
+            { data: 'rotation_period' },
+            { data: 'orbital_period' },
+            { data: 'diameter' },
+            { data: 'climate' },
+            { data: 'gravity'},
+            { data: 'terrain'},
+            { data: 'surface_water'},
+            { data: 'population'}
+        ]
+    } );
+});
